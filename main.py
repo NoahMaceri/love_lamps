60a782689912a12ed2e78a381cc3b6b35836fa83
Noah Maceri <noah.maceri@gmail.com>
2021-12-21 14:16:19 -0500

    Backlight switch update
    Switched from NPN backlight management to board management

--- main.py
+++ main.py
@@ -15,7 +15,6 @@ import smtplib, ssl, imaplib, time, board, neopixel, threading, digitalio, email
 
 #The GPIO is setup as BCM through the LCD init function, use GPIO names not pins numbers
 
-
 #LCD SETUP
 i2C_LCD = False
 
@@ -31,15 +30,10 @@ if i2C_LCD:
     i2c = busio.I2C(board.SCL, board.SDA)
 
     # Setup LCD screen
-    lcd = character_lcd.Character_LCD_I2C(i2c, lcd_columns, lcd_rows)
+    lcd = character_lcd.Character_LCD_I2C(i2c, lcd_columns, lcd_rows, address=0x27)
 
 else:
     import adafruit_character_lcd.character_lcd as characterlcd
-
-    # NPN transistor - Emitter connected to ground of LCD backlight, Base connected to GPIO 2, Collector connected to ground
-    # This effectively toggles the backlight on and off on the LCD
-    lcd_backlight = 2
-    GPIO.setup(lcd_backlight, GPIO.OUT)
 
     # LCD Pin out
     lcd_rs = digitalio.DigitalInOut(board.D25)
@@ -48,12 +42,14 @@ else:
     lcd_d5 = digitalio.DigitalInOut(board.D17)
     lcd_d6 = digitalio.DigitalInOut(board.D18)
     lcd_d7 = digitalio.DigitalInOut(board.D22)
+    lcd_backlight = digitalio.DigitalInOut(board.D2)
 
     # Setup LCD screen (disable blink and cursor bc its annoying)
-    lcd = characterlcd.Character_LCD_Mono(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, lcd_columns, lcd_rows)
+    lcd = characterlcd.Character_LCD_Mono(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, lcd_columns, lcd_rows, lcd_backlight, True)
 
 #General LCD Setup
 lcd.clear()
+lcd.backlight = False
 lcd.blink = False
 lcd.cursor = False
 
@@ -95,23 +91,11 @@ deviceUser = userList[0]
 targetUserIndex = 0 #Now this might seem stupid but trust me, it is
 targetUser = userList[targetUserIndex]
 
-#Backlight toggle
-def backlight_toggle(state):
-    if i2C_LCD and state:
-        lcd.backlight = True
-    elif i2C_LCD and not state:
-        lcd.backlight = False
-
-    if not i2C_LCD and state:
-        GPIO.output(lcd_backlight, True)
-    elif not i2C_LCD and not state:
-        GPIO.output(lcd_backlight, False)
-
 #Tab through users
 def user_button(channel):
     global targetUserIndex
     global targetUser
-    backlight_toggle(True)
+    lcd.backlight = True
     if targetUserIndex == 3:
         targetUserIndex = 0
     else:
@@ -123,7 +107,7 @@ def user_button(channel):
 #Tab through messages
 def message_button(channel):
     global message_index
-    backlight_toggle(True)
+    lcd.backlight = True
     if message_index == 8:
         message_index = 0
     else:
@@ -134,7 +118,7 @@ def message_button(channel):
 #When the enter button is pressed it displays what is being sent, sends it, then lets the user know
 def send_button(channel):
     global message_index
-    backlight_toggle(True)
+    lcd.backlight = True
     lcd.clear()
     lcd.message = "To: " + targetUser + "\n" + message_list[message_index]
     time.sleep(0.25)
@@ -170,7 +154,7 @@ def off():
     global pixels
     pixels.fill((0, 0, 0))
     pixels.show()
-    backlight_toggle(False)
+    lcd.backlight = False
     lcd.clear()
 
 #So here's the thing, this code is hot garbage and has band-aids on band-aids but guess what it works
@@ -178,8 +162,8 @@ if __name__ == "__main__":
 
     #Version info
     lcd.clear()
-    backlight_toggle(True)
-    lcd.message = "  Message Lamp\n  Version 0.95"
+    lcd.backlight = True
+    lcd.message = "  Message Lamp\n  Version 1.05"
     time.sleep(5)
 
     #Turn off LED and clear LCD
@@ -220,7 +204,7 @@ if __name__ == "__main__":
         if oldEmail != fullEmail:
             #check for receiver name, if not found, do not update
             if emailSplit[0] == deviceUser:
-                backlight_toggle(True)
+                lcd.backlight = True
                 #Animations have to be recreated every time bc of the animation library
                 if latestEmail.find(message_list[0]) > 0: #I love you
                     heartbeat_pulse = Pulse(pixels, speed=0.01, color=RED, period=1.25)
